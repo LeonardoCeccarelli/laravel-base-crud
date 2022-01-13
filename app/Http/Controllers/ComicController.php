5fdd809924b4ed1583e7262badb80b959eaeb20a
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-13 15:03:36 +0100

    add method destroy for delete item

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -116,6 +116,10 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+
+        $comic->delete();
+
+        return redirect()->route("comics.index");
     }
 }
